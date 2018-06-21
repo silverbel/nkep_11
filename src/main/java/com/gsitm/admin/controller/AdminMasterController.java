58242ea0cd801b3cbc9957616b77a18dbd6d8df8
silverbel <stackover92@gmail.com>
2018-06-21 12:18:17 +0900

    0621 1218

--- src/main/java/com/gsitm/admin/controller/AdminMasterController.java
+++ src/main/java/com/gsitm/admin/controller/AdminMasterController.java
@@ -1,24 +1,33 @@
 package com.gsitm.admin.controller;
 
+import java.util.List;
+
 import javax.annotation.Resource;
+import javax.inject.Inject;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.gsitm.admin.service.NoticeService;
+import com.gsitm.common.dto.NoticeDTO;
 import com.gsitm.common.dto.WorkSpaceDTO;
 import com.gsitm.user.service.WorkSpaceService;
 
 @Controller
 public class AdminMasterController {
 
+	@Inject
+	private NoticeService noticeService;
+	
 	@Resource(name="wServiceForUser")
 	private WorkSpaceService wService;
 	
 	@RequestMapping("/admin.do")
-	public ModelAndView adminIndex(ModelAndView mv, WorkSpaceDTO workSpaceDTO) {
+	public ModelAndView adminIndex(ModelAndView mv, NoticeDTO noticeDTO) {
+		List<NoticeDTO> list = noticeService.getNoticeList(noticeDTO);
 		mv.setViewName("/admin/index");
-		mv.addObject("workSpaceList", wService.getWorkSpaceList(workSpaceDTO));
+		mv.addObject("noticeList", list);
 		return mv;
 	}
 	
@@ -29,8 +38,10 @@ public class AdminMasterController {
 	}
 	
 	@RequestMapping("/mAdmin.do")
-	public ModelAndView mAdminIndex(ModelAndView mv) {
+	public ModelAndView mAdminIndex(ModelAndView mv, NoticeDTO noticeDTO) {
+		List<NoticeDTO> list = noticeService.getNoticeList(noticeDTO);
 		mv.setViewName("/mAdmin/index");
+		mv.addObject("noticeList", list);
 		return mv;
 	}
 }
